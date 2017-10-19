1be1b2f1433934ab6949b8bf354a8ae45d7e3ffc
JR-Bradley <32311147+JR-Bradley@users.noreply.github.com>
2017-10-19 14:01:09 -0600

    morestuff

---
+++ BMICalculator/BMICalculator/ViewController.designer.cs
@@ -0,0 +1,59 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace BMICalculator
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel bmiLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CalculateButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField heightTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField weightTextField { get; set; }
+
+        [Action ("UIButton781_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton781_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (bmiLabel != null) {
+                bmiLabel.Dispose ();
+                bmiLabel = null;
+            }
+
+            if (CalculateButton != null) {
+                CalculateButton.Dispose ();
+                CalculateButton = null;
+            }
+
+            if (heightTextField != null) {
+                heightTextField.Dispose ();
+                heightTextField = null;
+            }
+
+            if (weightTextField != null) {
+                weightTextField.Dispose ();
+                weightTextField = null;
+            }
+        }
+    }
+}
